38c7478c5beadf2d1536365b390a49a258e3733c
otman lahreche <otman.lahreche@gmail.com>
2023-08-06 12:09:11 +0100

    update settings

--- EPSP_site/EPSP_site/settings.py
+++ EPSP_site/EPSP_site/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'EPSP_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': env("DB_NAME"),
         'USER': env("DB_USER"),
         'PASSWORD': env("DB_PASSWORD"),
