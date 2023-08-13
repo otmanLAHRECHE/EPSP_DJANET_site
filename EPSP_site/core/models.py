d0142f5000b34cc71c5c9e85f09a01937a7d24d4
otman lahreche <otman.lahreche@gmail.com>
2023-08-13 10:49:11 +0100

    update

--- EPSP_site/core/models.py
+++ EPSP_site/core/models.py
@@ -1,16 +1,26 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from .manager import UserManager
+
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 
 
 
-class User(AbstractUser):
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=254, unique=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    email = models.CharField(max_length=255, unique=True)
-    password = models.CharField(max_length=255)
-    is_ambassador = models.BooleanField(default=True)
-    username = None
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    last_login = models.DateTimeField(null=True, blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
+    objects = UserManager()
+
+    def get_absolute_url(self):
+        return "/users/%i/" % (self.pk)
